5d53f30edaf277ef934196d3cddad12857c26933
rheera <rheera2@uwo.ca>
2023-09-23 15:00:07 -0400

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import { reviews } from "./assets/data/reviews";
 import "./App.css";
 
 function App() {
+  const [allReviews, setAllReviews] = useState(reviews);
+
+  const displayReviews = allReviews.map((review) => (
+    <h5 key={review.name} className="review-user">
+      {review.name}
+    </h5>
+  ));
+
   return (
     <>
       <Navbar />
       <div className="container">
         <h5 id="reviews"></h5>
+        {displayReviews}
       </div>
     </>
   );
